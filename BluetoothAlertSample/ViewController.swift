dce48d82d256982ebb4c189a31be22fdb56f4630
kntkymt <kntkc0de@gmail.com>
2021-11-12 16:10:20 +0900

    initial commit

---
+++ BluetoothAlertSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothAlertSample
+//
+//  Created by kntk on 2021/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
